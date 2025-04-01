c65dfeb8ba3780d382156ec79858c12fb45f582b
Lê Ngô Thúy Cúc <lengothuycuc92@gmail.com>
2025-04-01 18:04:28 +0700

    Add files via upload

--- Lesson3-groupby-orderby.sql
+++ Lesson3-groupby-orderby.sql
@@ -1,9 +1,14 @@
-SELECT COUNT(title) AS titles, type -- reveal a total number of title lines
-FROM titles
-GROUP BY type
-ORDER BY COUNT(title) DESC -- ascending (ASC) / descending (DESC)
+----SELECT COUNT(title) AS titles, type -- reveal a total number of title lines
+----FROM titles
+----GROUP BY type
+----ORDER BY COUNT(title) DESC -- ascending (ASC) / descending (DESC)
 
-SELECT *
-FROM titles;
+--SELECT COUNT(price) AS titles, price
+--FROM titles
+--GROUP BY price
+--ORDER BY price DESC;\
 
--- 2 select groups: reveal 2
+SELECT city, COUNT(au_id) as authors
+FROM authors
+GROUP BY city
+ORDER BY COUNT(au_id) DESC, city;
